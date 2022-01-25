72c11c1c1a10290cd6bd11a460f2b4e6bd2e2d98
nareshgopal <gnareshdpi@gmail.com>
2022-01-25 11:25:14 +0530

    History mode is turned off for Vue Router

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,28 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
+import Vue from "vue";
+import VueRouter from "vue-router";
 import Doggie from "../Views/Doggie.vue";
 import Kitten from "../Views/Kitten.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
-    {
-        path: '/',
-        redirect: '/doggie'
-    },
-    {
-        path: '/doggie',
-        name: "Doggie",
-        component: Doggie
-    },
-    {
-        path: '/kitten',
-        name: "Kitten",
-        component: Kitten
-    },
+  {
+    path: "/",
+    redirect: "/doggie",
+  },
+  {
+    path: "/doggie",
+    name: "Doggie",
+    component: Doggie,
+  },
+  {
+    path: "/kitten",
+    name: "Kitten",
+    component: Kitten,
+  },
 ];
 
 export default new VueRouter({
-    mode: "history",
-    base: process.env.BASE_URL,
-    routes
-})
+  base: process.env.BASE_URL,
+  routes,
+});
